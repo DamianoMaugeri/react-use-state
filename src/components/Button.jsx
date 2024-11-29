0337e8bfa85df1065763afdcedca66c131b372c9
DamianoMaugeri <damianomaugeri97@icloud.com>
2024-11-29 18:06:42 +0100

    [add] comportamento reattivo

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,7 @@
 import style from './Button.module.css'
-export default function Button({ language }) {
-
+export default function Button({ language, onSelect, current }) {
     return (
-        <button className={style.btn}>
+        <button onClick={() => onSelect()} className={`${style.btn} ${current ? style.current : ''}`}>
             {language.title}
         </button>
     )
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,24 +1,54 @@
 import { languages } from "../assets/languages";
 import Button from "./Button.jsx";
 import Card from "./Card.jsx";
+import { useState } from "react";
+
 
 
 export default function Main() {
 
+
+    const [selectedLanguage, setSelectedLanguage] = useState(null)
+
+    function updateSelectedLanguage(languageName) {
+
+        setSelectedLanguage((oldLanguageName) => {
+            if (languageName === oldLanguageName) {
+                return null;
+            } else {
+                return languageName;
+            }
+        });
+    }
+
+
+
     return (
         <main>
+            {/* <div>{selectedLanguage}</div> */}
             <div className="container main_list">
                 <ul className="button_list">{languages.map((language) => (
 
                     <li key={language.id}>
-                        <Button language={language} />
+                        <Button language={language} current={language === selectedLanguage ? true : false} onSelect={() => updateSelectedLanguage(language)} />
+                        {console.log(selectedLanguage)}
                     </li>
                 ))}
 
                 </ul>
+
             </div>
             <div className="container main_card">
-                <Card language={''} />
+                {
+                    selectedLanguage ?
+
+                        <Card language={selectedLanguage} /> :
+
+                        <div className="card">
+                            <h3>Nessul Linguaggio Selezionato </h3>
+                        </div>
+                }
+
             </div>
         </main>
     )
